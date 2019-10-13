f0c758bb9007de754f215fa2d47c336468390802
Mohamed Fathy <codetwostart@gmail.com>
2019-10-13 23:09:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace jaggedarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int[][] numbers = new int[3][];
+            numbers[0] = new int[3];
+            numbers[1] = new int[2];
+            numbers[2] = new int[1];
+            //initializ 
+            numbers[0][1] = 3; */
+            int[][] numbers = new int[][]{new int[]{1,2},new int[]{3,4,5},new int[]{6}};
+            for(int i = 0; i < numbers.Length;i++){
+                for(int j = 0; j < numbers[i].Length;j++){
+                    Console.WriteLine(numbers[i][j]);
+                }
+            }
+
+        }
+    }
+}
